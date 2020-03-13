b7551ecf4aa938f5f65a756991e0fc75e5fbb7db
afinesami <samibenomar84@gmail.com>
2020-03-13 20:26:56 +0000

    binary tree insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,28 +12,28 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *nodo;
 
-    if (parent == NULL)
-    return (NULL);
+if (parent == NULL)
+return (NULL);
 
-    nodo = calloc(1, sizeof(binary_tree_t));
-    if (nodo == NULL)
-    return (NULL);
-    if (parent->left == NULL)
-    {
-        parent->left = nodo;
-        nodo->parent = parent;
-        nodo->n = value;
-        nodo->right = NULL;
-        nodo->left = NULL;
-    }
-    else
-    {
-        nodo->left = parent->left;
-        parent->left->parent = nodo;
-        parent->left = nodo;
-        nodo->n = value;
-        nodo->parent = parent;
-        nodo->right = NULL;
-    }
-    return (nodo);
+nodo = calloc(1, sizeof(binary_tree_t));
+if (nodo == NULL)
+return (NULL);
+if (parent->left == NULL)
+{
+parent->left = nodo;
+nodo->parent = parent;
+nodo->n = value;
+nodo->right = NULL;
+nodo->left = NULL;
+}
+else
+{
+nodo->left = parent->left;
+parent->left->parent = nodo;
+parent->left = nodo;
+nodo->n = value;
+nodo->parent = parent;
+nodo->right = NULL;
+}
+return (nodo);
 }
