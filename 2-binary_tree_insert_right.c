06fff1dbb1a9b8f67d0f46cba2e2b4296c495206
afinesami <samibenomar84@gmail.com>
2020-03-13 20:32:44 +0000

    binary tree insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -
+ *
+ * Return: whatever 
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *node_right;
+
+if (parent == NULL)
+return (NULL);
+
+node_right = binary_tree_node(parent, value);
+if (node_right == NULL)
+return (NULL);
+if (parent->right)
+{
+parent->right->parent = node_right;
+node_right->right = parent->right;
+}
+parent->right = node_right;
+return (node_right);
+}
