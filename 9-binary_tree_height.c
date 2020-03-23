6b16859b41799a6351ef780b5edc0e424cfa3eab
afinesami <samibenomar84@gmail.com>
2020-03-23 20:46:32 +0000

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,20 +11,20 @@ size_t binary_tree_height2(const binary_tree_t *tree);
 
 size_t binary_tree_height2(const binary_tree_t *tree)
 {
-  int cont1 = 0, cont2 = 0;
+int cont1 = 0, cont2 = 0;
 
-  if (tree == NULL)
-    return (0);
+if (tree == NULL)
+return (0);
 
-  if (tree->left != NULL)
-    cont1 += binary_tree_height2(tree->left);
+if (tree->left != NULL)
+cont1 += binary_tree_height2(tree->left);
 
-  if (tree->right != NULL)
-    cont2 += binary_tree_height2(tree->right);
+if (tree->right != NULL)
+cont2 += binary_tree_height2(tree->right);
 
-  if (cont1 > cont2)
-    return (cont1 + 1);
-  return (cont2 + 1);
+if (cont1 > cont2)
+return (cont1 + 1);
+return (cont2 + 1);
 }
 
 /**
@@ -37,8 +37,8 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
+if (tree == NULL)
+return (0);
 
-  return ((binary_tree_height2(tree)) - 1);
+return ((binary_tree_height2(tree)) - 1);
 }
