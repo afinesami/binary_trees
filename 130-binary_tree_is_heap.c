219e301d3e8dbad11bd0207f6a56d519c869732e
afinesami <samibenomar84@gmail.com>
2020-03-23 21:45:04 +0000

    binary tree is heap

---
+++ 130-binary_tree_is_heap.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ *
+ *@tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid Max Binary Heap, and 0 otherwise
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (1);
+}
