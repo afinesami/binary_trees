589d3ed640026f540006c1bb1698c4800c6eba35
afinesami <samibenomar84@gmail.com>
2020-03-23 20:50:06 +0000

    binary tree nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,12 +7,12 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  size_t counter = 0;
+size_t counter = 0;
 
-  if (tree == NULL)
-    return (0);
-  if (tree->right != NULL || tree->left != NULL)
-    counter += binary_tree_nodes(tree->left) + 1
-      + binary_tree_nodes(tree->right);
-  return (counter);
+if (tree == NULL)
+return (0);
+if (tree->right != NULL || tree->left != NULL)
+counter += binary_tree_nodes(tree->left) + 1
++ binary_tree_nodes(tree->right);
+return (counter);
 }
